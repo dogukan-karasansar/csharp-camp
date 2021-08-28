8327a53012b2653000262fa669889ce0b6fbb11c
karasansar <55123100+dogukan-karasansar@users.noreply.github.com>
2021-08-28 21:38:09 +0300

    c# arrays

--- csharpCamp/Program.cs
+++ csharpCamp/Program.cs
@@ -6,15 +6,11 @@ namespace csharpCamp
     {
         public static void Main(string[] args)
         {
-            string name;
-            int age;
-            Console.Write("Name: ");
-            Console.WriteLine();
-            name = Console.ReadLine();
-            Console.Write("Age: ");
-            Console.WriteLine();
-            age = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Mr. " + name + " you are " + age + " years old");
+            string[] cars = { "bmw", "opel", "mercedes" };
+            for(int i = 0; i < cars.Length; i++)
+            {
+                Console.WriteLine(cars[i]);
+            }
         }
     }
 }
