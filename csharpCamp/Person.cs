7ceb988a2bf757ce7700a4c5c2ea498b67ecbb3c
karasansar <55123100+dogukan-karasansar@users.noreply.github.com>
2021-08-29 03:21:58 +0300

    properties

---
+++ csharpCamp/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace csharpCamp
+{
+    public class Person
+    {
+        public string name
+        { get; set; }
+
+        private int age;
+        public int Age
+        { get => age; set => age = value; }
+
+        public Person()
+        {
+        }
+    }
+}
--- csharpCamp/Program.cs
+++ csharpCamp/Program.cs
@@ -9,13 +9,12 @@ namespace csharpCamp
         public static void Main(string[] args)
         {
             Car opel = new Car("Astra", 2011, "Siyah");
+            Person person = new Person();
+            person.name = "Doğukan";
+            person.Age = 19;
+            Console.WriteLine(person.name);
             
-            Console.WriteLine(opel.color);
-
-           
-
-
-        
+            Console.WriteLine(opel.color);        
 
         }
     }
